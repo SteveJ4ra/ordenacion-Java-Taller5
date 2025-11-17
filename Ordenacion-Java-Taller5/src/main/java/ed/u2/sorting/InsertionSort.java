f37e8b10c4511c21a98ba88f530b296a09dc7b2e
ServioVega <servio.vega@unl.edu.ec>
2025-11-16 22:29:46 -0500

    Subo a projecto ordenacion java taller 5, las clases Insertion Sort y SortingDemo, 2 de las 5 clases que contiene este proyecto.

--- Ordenacion-Java-Taller5/src/main/java/ed/u2/sorting/InsertionSort.java
+++ Ordenacion-Java-Taller5/src/main/java/ed/u2/sorting/InsertionSort.java
@@ -1,7 +1,42 @@
 package ed.u2.sorting;
 
+import java.util.Arrays;
 
 public final class InsertionSort {
 
+    public static void sort(int[] array) {
+        sort(array, false);
+    }
 
+    public static void sort(int[] array, boolean trace) {
+
+        // Llamamos al validador de Utils.
+        // Si retorna 'false', nos detenemos (es un caso borde).
+        if (!SortingUtils.preSortValidation(array, trace, "InsertionSort")) {
+            return;
+        }
+
+        // Si llegamos aquí, el arreglo es válido y > 1
+        int arrayLength = array.length;
+
+        // Bucle externo: comienza desde el segundo elemento (índice 1)
+        for (int currentIndex = 1; currentIndex < arrayLength; ++currentIndex) {
+            int keyToInsert = array[currentIndex];
+            int scanIndex = currentIndex - 1;
+
+            /*
+             * Mueve los elementos de array[0..currentIndex-1] que son mayores que
+             * la 'keyToInsert', una posición hacia adelante de su posición actual.
+             */
+            while (scanIndex >= 0 && array[scanIndex] > keyToInsert) {
+                array[scanIndex + 1] = array[scanIndex];
+                scanIndex = scanIndex - 1;
+            }
+            array[scanIndex + 1] = keyToInsert;
+
+            if (trace) {
+                System.out.println("Iteración " + currentIndex + " (insertando " + keyToInsert + "): " + Arrays.toString(array));
+            }
+        }
+    }
 }
